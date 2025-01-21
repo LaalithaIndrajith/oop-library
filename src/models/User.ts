d241bdde64da5a4fefa4de9d60da1e8d55a71968
LaalithaIndrajith <laalithagajanayake@gmail.com>
2025-01-21 10:54:05 +0530

    feat: implement getters and setters for private properties to improve encapsulation
    - Refactored class properties to be private for better data protection.
    - Added public getter and setter methods to allow controlled access and modification of private properties.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,27 @@
 import {LibraryCatalogue} from "./LibraryCatalogue";
 
 export class User {
-    protected name: string;
-    protected email: string;
+    private name: string;
+    private email: string;
 
     constructor(name: string, email: string) {
         this.name = name;
+        this.email = email;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public updateName(name: string) {
+        this.name = name;
+    }
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+    public updateEmail(email: string) {
         this.email = email;
     }
 
